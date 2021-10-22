c7b3c9330c64ae856517b4f979178a230bb39f24
Goncalo Pinto <goncalojlpinto@gmail.com>
2021-10-22 22:32:29 +0100

    routes created

--- index.js
+++ index.js
@@ -6,4 +6,25 @@ app.get("/", (req, rep) => {
     rep.send("App Init TEST")
 });
 
+app.get("/appointments", (req, res) => {
+    res.json([{Year: "2020", Mouth: "July", Hour: "15:00"}])
+})
+
+app.get("/appointments/:agent", (req, res) => {
+    res.send(`Returns the agent with ID: ${req.params.agent}`)
+})
+
+app.post("/appointments", (req, res) => {
+    console.log(req.body)
+    res.send("Posts an Appointment")
+})
+
+app.put("/appointments/:id", (req, res) => {
+    res.send("Updates an appointment")
+})
+
+app.delete("/appointment/:id", (req, res) => {
+    res.send("Deletes an appointment")
+})
+
 app.listen(PORT, () => { console.log(`Server is running on port: ${PORT}`) });
