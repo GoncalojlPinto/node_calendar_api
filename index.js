37f2fefcae505d91098255a540d38b8f39b2149a
Goncalo Pinto <goncalojlpinto@gmail.com>
2021-11-08 21:07:27 +0000

    added env config

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 const apiRouter = require('./routes/api');
 const defaultRouter = require('./routes/default');
-const auth = require('./routes/auth');
-const app = express();
-const jwt = require('jsonwebtoken');
+const authRouter = require('./routes/auth');
 const PORT = 5005;
+require("dotenv").config();
 
-require("dotenv-safe").config();
 
 
 //DB Connection
-mongoose.connect('mongodb://localhost:27017/Calendar');
+mongoose.connect('mongodb://localhost:27017/Calendar', () => console.log('Connected to MongoDB'));
 
 //Express Configs
-app.use('/auth', auth);
 app.use(express.json());
+app.use('/api/user', authRouter);
 app.use('/api', apiRouter);
 app.use('/', defaultRouter);
+
 
 
 app.listen(PORT, () => { console.log(`Server is running on port: ${PORT}`) });
