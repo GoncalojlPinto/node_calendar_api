f9e8402bd286ff78d3e79fd7de0e34ca03794cb8
Goncalo Pinto <goncalojlpinto@gmail.com>
2021-11-08 21:11:05 +0000

    updated appoint route with authorization

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const appointmentController = require("../controllers/appointmentController");
 const router = express.Router();
+const verifyToken = require('../verifyToken');
 
 
-router.get("/appointments", appointmentController.index)
-router.get("/appointments/:id", appointmentController.show)
+router.get("/appointments", verifyToken, appointmentController.index)
+router.get("/appointments/:id", verifyToken, appointmentController.show)
 router.post("/appointments", appointmentController.create)
-router.put("/appointments/:id", appointmentController.update)
-router.delete("/appointments/:id", appointmentController.destroy)
+router.put("/appointments/:id", verifyToken,  appointmentController.update)
+router.delete("/appointments/:id", verifyToken, appointmentController.destroy)
 
 module.exports = router;
